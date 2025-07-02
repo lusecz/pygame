0575d7604d0117b63c1ef2215f642d25c7fcfb4c
lusecz <99739250+lusecz@users.noreply.github.com>
2025-07-02 17:50:04 -0300

    segundo

---
+++ blocos.py
@@ -0,0 +1,12 @@
+from corbloco import Colors
+
+class Block:
+    def __init__(self, id):
+        self.id = id
+        self.cells = {}
+        self.cell_size = 30
+        self.rotation_state = 0
+        self.colors = Colors.get_cells_colors()
+
+    def draw(self, screen):
+        
--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 import pygame,sys
+from grid import Grid
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -6,7 +7,17 @@ dark_blue = (44, 44, 127)
 screen = pygame.display.set_mode((300,600))
 pygame.display.set_caption("Pygame Tetris")
 
+
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+#DESCOMENTAR PARA VER AS GRADES SENDO PREENCHIDAS 
+#game_grid.grid[0][0]=1
+#game_grid.grid[3][5]=4
+#game_grid.grid[17][5]=7
+
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +25,9 @@ while True:
             pygame.quit()
             sys.exit()
         #Desenhando
-        screen.fill(dark_blue)
-        pygame.display.update()
-        clock.tick(60)
+    screen.fill(dark_blue)
+    game_grid.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
+
